88b829204e0ebd0197095e091c293199e2fc2ecf
Rehan-111 <147245549+Rehan-111@users.noreply.github.com>
2026-06-20 17:10:23 +0530

    Created AdminDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react"
 import Login from './components/Auth/Login'
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard"
 import TaskAssign from "./components/other/TaskAssign"
+import Header from "./components/other/Header"
+import AdminDashboard from "./components/Dashboard/AdminDashboard"
 const App=() => {
 
   return (
     <>
     {/* <Login/> */}
-    <EmployeeDashboard/>
-    <TaskAssign/>
+    {/* <EmployeeDashboard/> */}
+    <AdminDashboard/>
     </>
   )
 }
